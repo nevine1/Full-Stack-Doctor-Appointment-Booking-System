328ac2c498a1014d68a142d4d0091de9a40e3de4
nevine1 <venasamaan@gmail.com>
2025-06-30 21:04:21 -0400

    Hello , this is the first commit for my new project, doctor appointment booking system

---
+++ front-end/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-blue-200 m-10 p-10 rounded-lg">
+      <h1 className="text-[40px] bold text-red-700">Hello next js website</h1>
+    </div>
+  );
+}
