21c5cc31116b4546c8d0e026a0b1ba9c8f605886
nevine1 <venasamaan@gmail.com>
2025-07-01 22:18:41 -0400

    few changes for navbar links done

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,14 +1,18 @@
 "use client"
-import React from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image';
 import { assets } from '../assets/assets'
 import { useRouter } from 'next/navigation'
+import { IoIosArrowDown } from "react-icons/io";
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(false); //if it is true , it means the user is logged in
   const router = useRouter(); 
   const handleClick = () => {
     router.push('/auth/login')
   }
+
   return (
       <div className="flex flex-row  justify-between items-center py-4 mx-10 
         border-b border-gray-200 bg-brandBlue">
@@ -51,11 +55,41 @@ const Navbar = () => {
             </ul>
 
 
-          <div>
+      <div>
+        {
+          token ? (
+            <div className="flex gap-2 items-center group relative"> 
+              <Image
+                src={assets.profile_pic}
+                alt="profile pic"
+                width={32}
+                height={32}
+                className="rounded-full w-8"
+              />
+              <IoIosArrowDown onClick={() => setShowMenu(!showMenu)}
+                className="text-[20px] cursor-pointer"
+              />
+              {
+                showMenu && (
+                  <div className={`absolute z-20 right-0 top-0 pt-14  text-slate-700  hidden group-hover:block  transition-all duration-400 `}>
+                    <div className="w-48  flex flex-col gap-3 rounded-md bg-slate-100 p-4 shadow-lg">
+                      <p className='cursor-pointer hover:text-black' onClick={()=>router.push('/auth/profile')}>My Profile</p>
+                      <p className='cursor-pointer hover:text-black' onClick={()=>router.push('/myAppointment')}>My Appointments</p>
+                      <p className='cursor-pointer hover:text-black' onClick={()=>setToken(false)}>Logout</p>
+                    </div>
+                  </div>
+                )
+              }
+              
+            </div>
+          ): (
             <button onClick={handleClick}
               className="bg-blue-500 text-white px-8 rounded-full py-1 hidden md:block">
-                  Login
+                 Create account
               </button>
+          )
+        }
+            
           </div>
                 
     </div>
